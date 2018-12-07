cae81e756942eb03c4de6b097125017f755d04d2
vlastik <vlastikczech1@gmail.com>
2018-12-06 17:59:34 -0700

    redesigned the application

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+server = app.listen(3000);
+
+const io = require("socket.io")(server)
+
+io.on('connection', (socket) => {
+    console.log('New user connected')
+
+    socket.username = "Anonymous";
+
+    socket.on('change_name', (data) => {
+        socket.username = data.username
+    })
+
+    socket.on('new_message', (data) => {
+        socket.emit('new_message', {message : data.message, username: socket.username})
+    })
+})
---
+++ public/chat.js
@@ -0,0 +1,25 @@
+$(function() {
+    var socket = io.connect('http://localhost:3000')
+
+    var message = $("#message")
+    var username = $("#username")
+    var send_message = $("#input_zone")
+    var send_username = $("#send_username")
+    var chatroom = $("#chatroom")
+
+    send_message.on('submit', function(e) {
+        socket.emit('new_message', {message : message.val()})
+        e.preventDefault();
+    })
+
+    socket.on("new_message", (data) => {
+        console.log(data)
+        message.val('');
+        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
+
+    send_username.click(function() {
+        console.log(username.val())
+        socket.emit('change_name', {username : username.val()})
+    })
+});
